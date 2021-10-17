0767043d9c16241c0dea07e779032029f6143566
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-10-17 10:57:41 +0900

    Initial Commit

---
+++ QiitaList/QiitaListApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaListApp.swift
+//  QiitaList
+//
+//  Created by 神山賢太郎 on 2021/10/17.
+//
+
+import SwiftUI
+
+@main
+struct QiitaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
